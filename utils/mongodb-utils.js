74925be96f9cdd2c5b37c4bc42ba6460ad9d2600
Tay <tcraig941@gmail.com>
2023-11-06 20:04:24 -0500

    removed junk and updated db pass

--- utils/mongodb-utils.js
+++ utils/mongodb-utils.js
@@ -2,7 +2,7 @@ import { MongoClient } from "mongodb";
 
 export async function connectDatabase() {
     const client = await MongoClient.connect(
-        'mongodb+srv://jsdevtay23:Ca2253ke@cluster0.txe4anx.mongodb.net/?retryWrites=true&w=majority'
+        'mongodb+srv://jsdevtay23:Ca2253ke!!@cluster0.txe4anx.mongodb.net/?retryWrites=true&w=majority'
         );
     return client;
 }
